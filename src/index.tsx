72d9014195f4c662bda3f9744ae203dd9c5cc986
Swaraj Panigrahi <panigrahiswaraj89@gmail.com>
2017-12-06 23:19:37 +0530

    1. added font. 2. Making app SASS ready

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import 'typeface-lato';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(
   <App />,
